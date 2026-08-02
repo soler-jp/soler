e1ef448a707f6db28bcf018d336f82e772206705
soler-jp <172242354+soler-jp@users.noreply.github.com>
2026-08-02 14:40:07 +0900

    日本語のValidationエラーカタログファイルを追加

---
+++ lang/ja/validation.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'accepted' => ':attributeを承認してください。',
+    'array' => ':attributeは配列で入力してください。',
+    'boolean' => ':attributeは真偽値で入力してください。',
+    'date' => ':attributeは正しい日付を入力してください。',
+    'integer' => ':attributeは整数で入力してください。',
+    'in' => '選択された:attributeが不正です。',
+    'max' => [
+        'array' => ':attributeは:max件以下で入力してください。',
+        'numeric' => ':attributeは:max以下で入力してください。',
+        'string' => ':attributeは:max文字以内で入力してください。',
+    ],
+    'min' => [
+        'array' => ':attributeは:min件以上入力してください。',
+        'numeric' => ':attributeは:min以上で入力してください。',
+        'string' => ':attributeは:min文字以上で入力してください。',
+    ],
+    'nullable' => ':attributeはnullを指定できます。',
+    'required' => ':attributeは必須です。',
+    'string' => ':attributeは文字列で入力してください。',
+];
